6ada13f129923f83f45384ba9175409bed75276e
Henry Calderon <theceintiant@gmail.com>
2020-09-09 19:05:56 -0700

    Adding in the new views

---
+++ MicroMuse/Controllers/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  MicroMuse
+//
+//  Created by Henry Calderon on 9/9/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
